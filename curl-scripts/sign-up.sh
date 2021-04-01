5770ec3bd81a8405628c7900e46f175c1f299707
laurenmenendez <menendez98@gmail.com>
2021-04-01 13:39:17 -0400

    Create sign-up curl, forms in html, update config

---
+++ curl-scripts/sign-up.sh
@@ -0,0 +1,15 @@
+# VARIABLE=VALUE sh curl-scripts/sign-up.sh
+
+curl "https://tic-tac-toe-api-production.herokuapp.com/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD_CONFIRMATION}"'"
+    }
+  }'
+
+echo
