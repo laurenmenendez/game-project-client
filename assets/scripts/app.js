4cee7bf8279ace41ba6c0db117093d8d1f14f64f
laurenmenendez <menendez98@gmail.com>
2021-04-02 10:11:59 -0400

    Set up sep auth and game folders, gameboard html

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,13 @@
 'use strict'
 
-const events = require('./events')
+const events = require('./auth/events')
 
 $(() => {
+// auth events
   $('#sign-up-form').on('submit', events.onSignUp)
   $('#sign-in-form').on('submit', events.onSignIn)
   $('#change-password').on('submit', events.onChangePassword)
   $('#sign-out-button').on('click', events.onSignOut)
 })
+
+// game events
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const store = require('./store')
+const store = require('.././store')
 
 const onSignUpSuccess = function () {
   $('#messages').text('Thanks for signing up!')
